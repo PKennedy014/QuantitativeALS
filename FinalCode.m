d5954411df0bf6162efffdd8007c94ad8011a048
PKennedy014 <97614064+PKennedy014@users.noreply.github.com>
2022-03-31 10:50:29 -0400

    Add files via upload

--- FinalCode.m
+++ FinalCode.m
@@ -1,35 +1,47 @@
-% functionSynapseandflux
+clc,clear, close all
+% BME4409 Final Project
+% Dr. Ferrall-Fairbanks
+% Members: Vanessea Cruz, Jeffrey Chen, Estefania Hernandez, Patrick Kennedy
+%Modeling the effect of ALS on muscle contraction
+
 %Parameters
-L=40*10^-3; %µm
-tfinal=4e-7; %seconds
-D=400; %µm^2/s
+L=40*10^-3; %Cleft distance µm 
+tfinal=4e-7; %seconds. For graph
+D=400; %ACh difusivity in NMJ µm^2/s
 tpoints=501;
 xpoints=41;
+
 %m=0 denotes that our problem is in a slab
 m = 0;
+
 %Create time and distance vectors
 x = linspace(0,L,xpoints);
 t = linspace(0,tfinal,tpoints);
+
 %Call pdepe to solve the equation
 sol = pdepe(m,@pdex1pde,@pdex1ic,@pdex1bc,x,t);
 %Solution is first component of sol
 C = sol(:,:,1);
 %Estimating flux at boundary
 Flux=zeros(1,length(t));
-for i=2:501
+for i=2:tpoints
 dFlux=-D*((C(i,41)-C(i,40))/(L/xpoints))*(tfinal/tpoints);
 Flux(i)=Flux(i-1)+dFlux;
 end
-%Plotting Flux vs time
+%Bound Receptor surface concentration
+BRSC = Flux*6.022e23;
+BRSCmax = max(BRSC);
+
+%Plotting BRSC vs time
 figure
-plot(t,Flux*6.022e23)
+plot(t,BRSC)
 xlabel('Time (seconds)')
 ylabel('Bound Receptor Surface Concentration (molecules/µm^2)')
 title('ACh Bound at Post-Synaptic Membrane')
 
+
 %PDE function (define our PDE)
 function [c,f,s] = pdex1pde(x,t,Conc,DCDx)
-15
 D=400; %µm^2/s
 k=23e7; %1/s, kcat of AChE
 %Time derivative coefficient
